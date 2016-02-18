901caa35c90b8b3758dd405d33dab62cf0cbcc08
GregChan <gregorywarrenchan@gmail.com>
2016-02-18 15:46:12 -0600

    added form validations

--- public/js/email.js
+++ public/js/email.js
@@ -1,38 +1,40 @@
-$(document).ready(function() {
-	$('[data-match]').hide();
-	$('[data-feedback]').hide();
-	$('select').material_select();
-});
-
-$('[data-template-select]').change(function() {
-	var value = $('[data-template-select]').val();
-	$('[data-match]').hide();
-	$('[data-feedback]').hide();
-	$('[data-' + value + ']').show();
-});
-
-$('[data-match-form]').submit(function() {
-	$('[data-kellogg]').val($('#kelloggEmail').val());
-	$('[data-prosp]').val($('#prospEmail').val());
-	console.log($('[data-prosp]').val());
-	console.log($('[data-kellogg]').val());
-	$.ajax({
-		type: "POST",
-		url: "/cat/mail/email",
-		data: $(this).serialize(),
-		success: function() {
-			alert('success');
-		}
+(function() {
+	$(document).ready(function() {
+		$('[data-match]').hide();
+		$('[data-feedback]').hide();
+		$('select').material_select();
 	});
-});
 
-$('[data-feedback-form]').submit(function() {
-	$.ajax({
-		type: "POST",
-		url: "/cat/mail/email",
-		data: $(this).serialize(),
-		success: function() {
-			alert('success');
-		}
+	$('[data-template-select]').change(function() {
+		var value = $('[data-template-select]').val();
+		$('[data-match]').hide();
+		$('[data-feedback]').hide();
+		$('[data-' + value + ']').show();
 	});
-});
+
+	$('[data-match-form]').submit(function() {
+		$('[data-kellogg]').val($('#kelloggEmail').val());
+		$('[data-prosp]').val($('#prospEmail').val());
+		console.log($('[data-prosp]').val());
+		console.log($('[data-kellogg]').val());
+		$.ajax({
+			type: "POST",
+			url: "/cat/mail/email",
+			data: $(this).serialize(),
+			success: function() {
+				alert('success');
+			}
+		});
+	});
+
+	$('[data-feedback-form]').submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "/cat/mail/email",
+			data: $(this).serialize(),
+			success: function() {
+				alert('success');
+			}
+		});
+	});
+}());

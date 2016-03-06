fd614db7a649e700157bf7702063b7b99249c479
shulerhl <hollidayshuler2016@u.northwestern.edu>
2016-03-05 22:35:04 -0600

    add user info at top of survey

--- resources/wild/pages/survey.js
+++ resources/wild/pages/survey.js
@@ -1,29 +1,49 @@
 var exports = module.exports = {},
     request = require('request');
+    async = require('async'),
 
 exports.path = 'survey';
 
 exports.getHandle = function(req, res) {
-    request({
-        url: process.env.BASE_URL + '/cat/community/1/profile-survey',
-        method: 'GET',
-        headers: {
-            'Cookie': 'userID=' + req.cookies.userID
-        }
-    }, function(error, response, body) {
-        var survey = JSON.parse(body);
-
-        if (req.cookies.userID != undefined && req.cookies.userID != "undefined") {
-            res.render('survey', {
-                survey: survey,
-                curUser: req.cookies.userID
+    if (req.cookies.userID != undefined && req.cookies.userID != "undefined") {
+        async.parallel([
+            function(callback) {
+                request({
+                    url: process.env.BASE_URL + '/cat/community/1/profile-survey',
+                    method: 'GET',
+                    headers: {
+                        'Cookie': 'userID=' + req.cookies.userID
+                    }
+                }, function(error, response, body) {
+                        callback(error, JSON.parse(body));
+                    });
+                },
+            function(callback) {
+                request({
+                    url: process.env.BASE_URL + '/cat/user/' + req.cookies.userID,
+                    method: 'GET',
+                    headers: {
+			'Cookie': 'userID=' + req.cookies.userID
+                    }
+                }, function(error, response, body) {
+                        callback(error, JSON.parse(body));
+                    });
+                }
+            ],
+            function(err, results) {
+                console.log(results[1]);
+                if (!err){
+                    res.render('survey', {
+                    survey: results[0],
+                    curUser: results[1]
+                    });
+                } else {
+                    res.status(500);
+                }
+                res.end();
             });
-            res.end();
-            return;
-
         } else {
             res.status(500);
             res.end();
-        }
-    });
+        }    
 }

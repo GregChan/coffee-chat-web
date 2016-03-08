49c51cc014a189f56bc13a9660644106e6f616f8
NDHApps <nhall7@comcast.net>
2016-03-08 10:38:21 -0600

    Added javascript to properly display profile, accept match, and rate with feedback

--- public/js/match.js
+++ public/js/match.js
@@ -4,22 +4,44 @@
 		var fname;
 		$.ajax({
 			type: 'GET',
-			url: '/cat/user/' + matchId,
+			url: '/cat/user/' + matchId + '/profile',
 			success: function(match) {
-			   console.log(match);
-			   $(".current-matches .match" + matchId + " #match-name").text(match.firstName + ' ' + match.lastName);
-			   $(".current-matches .match" + matchId + " #match-title").text(match.headLine);
-			   $(".current-matches .match" + matchId + " #match-link").attr("href",match.linkedInProfile);
-			   $(".current-matches .match" + matchId + " #match-pic").attr("src",match.profilePic);
-			   fname = match.firstName;
+                $(".current-matches .match" + matchId + " #match-name").text(match.firstName + ' ' + match.lastName);
+                $(".current-matches .match" + matchId + " #match-title").text(match.headLine);
+                $(".current-matches .match" + matchId + " #match-link").attr("href",match.linkedInProfile);
+                $(".current-matches .match" + matchId + " #match-pic").attr("src",match.profilePic);
+                if (match.positions.work) {
+                    for (var i = 0; i < match.positions.work.length; i++) {
+                        $(".current-matches .match" + matchId + " #work-container").append("<div class='title'>"+match.positions.work[i].title+"</div><div class='subtitle'>"+match.positions.work[i].company+"</div>");
+                    }
+                }
+                if (match.positions.education) {
+                    for (var i = 0; i < match.positions.education.length; i++) {
+                        $(".current-matches .match" + matchId + " #education-container").append("<div class='title'>"+match.positions.education[i].title+"</div><div class='subtitle'>"+match.positions.education[i].school+"</div>");
+                    }
+                }
 			}
 		});
 		$.ajax({
 			type: 'GET',
 			url: '/cat/user/community/1/' + matchId + '/profile',
 			success: function(profile) {
-                console.log(profile);
+                if (profile && profile.fields) {
+                    for (var i = 0; i < profile.fields.length; i++) {
+                        for (var j = 0; j < profile.fields[i].values.length; j++) {
+                            $(".current-matches .match" + matchId + " #interests-container").append("<div>#"+profile.fields[i].values[j].name+"</div>");
+                        }
+                    }
+                }
 			}
 		});
+        $('#accept-match').click(function(event){
+            $('#match-accept').hide();
+            $('#match-rate').show();
+        });
+        $('.rating a').click(function(event){
+            $(this).siblings('a').removeClass('active');
+            $(this).addClass('active');
+        });
 	});
 }());

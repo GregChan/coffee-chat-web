1642d771e539e0d774a22d33677ba068208fce99
shulerhl <hollidayshuler2016@u.northwestern.edu>
2016-03-08 13:59:32 -0600

    enterprise page, not connected to backend

---
+++ resources/wild/pages/enterprise.js
@@ -0,0 +1,9 @@
+var exports = module.exports = {};
+
+exports.path = 'enterprise';
+
+exports.getHandle = function(req, res) {
+    res.render('enterprise', {
+    	curUser: req.cookies.userID
+    });
+}

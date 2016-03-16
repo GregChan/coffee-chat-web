c3c917aa132523104b86c97847c6f4f8e78f963b
GregChan <gregorywarrenchan@gmail.com>
2016-03-16 05:50:13 -0500

    created analytics end point

---
+++ resources/cat/community/getCommunityAnalytics.js
@@ -0,0 +1,31 @@
+var exports = module.exports = {};
+var dbConn = require("../../elf/db/dbConn.js");
+
+exports.path = 'cat/community/:communityID/analytics/';
+
+exports.getHandle = function(req, res) {
+    // console.log('cat/community/:communityID/users: communityID: request received');
+    var communityID = req.params.communityID;
+    // console.log('cat/community/:communityID/users: communityID: ' + req.params.communityID);
+    getCommunityAnalytics(communityID, req, res);
+}
+
+function doPromise(p, callback) {
+    p.then(function(data) {
+        callback(null, data);
+    }).catch(function(reason) {
+        callback(reason, null);
+    });
+}
+
+function getCommunityAnalytics(communityID, req, res) {
+    var positions = req.body;
+    doPromise(dbConn.getCommunityAnalytics(communityID, positions), function(error, data) {
+        if (error) {
+            console.log(error);
+            res.sendStatus(error.error);
+        } else {
+            res.json(data);
+        }
+    }); 
+}

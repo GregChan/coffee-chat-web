2c7920bf88ed9184abbb41819d7d9d828b12e808
NDHApps <nhall7@comcast.net>
2016-04-12 12:17:00 -0500

    Added admin matching page

---
+++ resources/wild/pages/admin/adminMatch.js
@@ -0,0 +1,33 @@
+var exports = module.exports = {},
+	request = require('request'),
+	async = require('async');
+
+exports.path = 'admin/match';
+
+exports.getHandle = function(req, res) {
+	async.parallel([
+			function(callback) {
+				request({
+					url: process.env.BASE_URL + '/cat/community/1/users',
+					method: 'GET',
+					headers: {
+						'Cookie': 'userID=' + req.cookies.userID
+					}
+				}, function(error, response, body) {
+					callback(error, JSON.parse(body));
+				});
+			},
+		],
+		function(err, results) {
+			if (!err) {
+				console.log(results[0]);
+				var data = {
+					users: results[0],
+				}
+
+				res.render('admin-match', data);
+			} else {
+				res.sendStatus(500);
+			}
+		});
+}

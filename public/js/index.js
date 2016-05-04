b856b0f49855ae0030c01010039043833e7f2431
GregChan <gregorywarrenchan@gmail.com>
2016-05-04 16:04:18 -0500

    reorganize js for home, index, and signin and added redirect to admin page from signin

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,3 @@
 (function() {
-	$('[data-submit]').click(function(e) {
-		console.log("GO");
-		$.ajax({
-			url: "/wild/user/login",
-			method: "POST",
-			data: {
-				email: $('#email').val(),
-				pw: $('#password').val()
-			},
-			success: function(data) {
-				console.log(data);
-			}
-		});
-		$('#code').html('Please enter a valid company code');
-	});
+	$('.parallax').parallax();
 })();

78eadd2fba65de924f10b61616b3a215b44c4c36
shulerhl <hollidayshuler2016@u.northwestern.edu>
2016-06-03 14:19:04 -0500

    use mixpanel data to display graphs

--- public/js/admin-metrics.js
+++ public/js/admin-metrics.js
@@ -7,7 +7,9 @@ var dayPoints = [['Day', 'Total Matches']];
 var weekPoints = [['Week', 'Total Matches']];
 var monthPoints = [['Month', 'Total Matches']];
 var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
+var joinDataDay = [['Day', 'Total Signups']];
+var joinDataMonth = [['Month', 'Total Signups']];
+var sortDates = [];
 function main() {
     dayPoints.push([matches[0].matchTime.substring(0, 10), 1]);
     weekPoints.push([matches[0].matchTime.substring(0, 10), 1]);
@@ -31,6 +33,54 @@ function main() {
         }
     };
     displayDay();
+    for(var date in joinsDay){
+      sortDates.push(parseInt(date.substring(0, 4) + date.substring(5, 7) + date.substring(8, 10)));
+    }
+    sortDates.sort(function(a, b){return a-b});
+    for(var i = 0; i < sortDates.length; i++){
+      var date = sortDates[i].toString();
+      date = date.substring(0, 4) + '-' + date.substring(4, 6) + '-' + date.substring(6, 8);
+      joinDataDay.push([date.substring(5,10), joinsDay[date]]);
+    }
+    for(var date in joinsMonth){
+      sortDates.push(parseInt(date.substring(0, 4) + date.substring(5, 7) + date.substring(8, 10)));
+    }
+    sortDates.sort(function(a, b){return a-b});
+    for(var i = 0; i < sortDates.length; i++){
+      var date = sortDates[i].toString();
+      date = date.substring(0, 4) + '-' + date.substring(4, 6) + '-' + date.substring(6, 8);
+      joinDataMonth.push([date.substring(5,10), joinsMonth[date]]);
+    }
+    console.log(joinDataDay);
+    showJoins("day");
+}
+
+function showJoins(unit) {
+  if(unit == "day"){
+      var data = google.visualization.arrayToDataTable(joinDataDay);
+      var options = {
+            title: "Total Signups",
+            curveType: 'line',
+            legend: { position: 'none' },
+            colors: ['#ee6545'],
+        hAxis: {title: "Day"},
+        vAxis: {title: "Total Signups"}
+      };
+  }
+  else{
+      var data = google.visualization.arrayToDataTable(joinDataMonth);
+      var options = {
+            title: "Total Signups",
+            curveType: 'line',
+            legend: { position: 'none' },
+            colors: ['#ee6545'],
+        hAxis: {title: "Month"},
+        vAxis: {title: "Total Signups"}
+      };
+  }
+  
+  var chart = new google.visualization.LineChart(document.getElementById('joins_chart'));
+  chart.draw(data, options);
 }
 
 function displayDay() {
@@ -44,7 +94,7 @@ function displayDay() {
       vAxis: {title: "Total Matches"}
     };
     
-    var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+    var chart = new google.visualization.LineChart(document.getElementById('match_chart'));
     chart.draw(data, options);
 }
 
@@ -60,7 +110,7 @@ function displayWeek() {
       vAxis: {title: "Total Matches"}
     };
     
-    var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+    var chart = new google.visualization.LineChart(document.getElementById('match_chart'));
     chart.draw(data, options);
 }
 
@@ -76,6 +126,6 @@ function displayMonth() {
       vAxis: {title: "Total Matches"}
     };
     
-    var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+    var chart = new google.visualization.LineChart(document.getElementById('match_chart'));
     chart.draw(data, options);
 }
--- resources/wild/pages/admin/adminMetrics.js
+++ resources/wild/pages/admin/adminMetrics.js
@@ -1,10 +1,47 @@
 var exports = module.exports = {},
 	request = require('request'),
-	async = require('async');
+	async = require('async'),
+	MixpanelExport = require('mixpanel-data-export');
+
+panel = new MixpanelExport({
+  api_key: "7e6b48a33a7e531684aebc32711da3e2",
+  api_secret: "58626d12dc5dafe2c38bc7d3a2b9343d"
+});
 
 exports.path = 'admin/metrics';
 
+
 exports.getHandle = function(req, res) {
+	var action;
+	var joinsDay;
+	var joinsMonth;
+	if(req.communityID == '41'){
+		action = "join-community Deloitte";
+	}
+	else if(req.communityID == '31'){
+		action = "join-community FemaleFounders";
+	}
+	else{
+		action = "join-community MEM";
+	}
+	panel.segmentation({
+		event: action,
+		from_date: "2016-03-01",
+		to_date: "2016-06-01",
+		unit: "day"
+	}).then(function(data) {
+		joinsDay = data;
+	  	console.log(data);
+	});
+	panel.segmentation({
+		event: action,
+		from_date: "2016-03-01",
+		to_date: "2016-06-01",
+		unit: "month"
+	}).then(function(data) {
+		joinsMonth = data;
+	  	console.log(data);
+	});
 	async.parallel([
 			function(callback) {
 				request({
@@ -43,7 +80,7 @@ exports.getHandle = function(req, res) {
 				}, function(error, response, body) {
 					callback(error, JSON.parse(body));
 				});
-			},
+			}
 		],
 		function(err, results) {
 			if (!err) {
@@ -52,6 +89,8 @@ exports.getHandle = function(req, res) {
 					users: results[0],
 					metrics: results[1],
 					matches: results[2],
+					joinsDay: joinsDay["data"].values[action],
+					joinsMonth: joinsMonth["data"].values[action]
 				}
 
 				res.render('admin-metrics', data);

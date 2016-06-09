31a73dbd684a6efbc05da5ce618aa935d63ddc32
shulerhl <hollidayshuler2016@u.northwestern.edu>
2016-06-09 09:21:43 -0500

    Revert "server no longer breaks when password incorrect, user told to try again or success"
    This reverts commit 9c131625fe32a0e5e2d77ac543b55a50d99d7276.

--- public/js/signin.js
+++ public/js/signin.js
@@ -11,8 +11,7 @@
 			success: function(data) {
 				window.location = '/admin/talent';
 			}
-			$('#error').html('Invalid username or password');
 		});
-		
+		$('#code').html('Please enter a valid company code');
 	});
 })();
